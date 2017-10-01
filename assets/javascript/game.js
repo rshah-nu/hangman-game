93a06e204645da3767740c8f616a89d865df04e6
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-10-01 16:45:21 -0500

    Finalized game, removed copyrighted music prior to github upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,58 @@
 
 // Variable to store random words to guess in an array
 // var wordArray = ["Yesterday", "Blackbird", "Length", "Drops", "Elton", "Dog", "Table", "Awning", "Jeans", "Chalk", "Plants", "Ozone"];
-var wordArray = ["Yesterday I Saw"];
+var wordArray = ["Yesterday", "Hey Jude", "Here Comes The Sun", "Revolution", "Eleanor Rigby", "Come Together", "Across the Universe", "She Loves You", "All My Loving", "Helter Skelter"];
 // Maximum guesses available
 var guessesRemaining = 12;
+// HACK!
+document.getElementById("guessesRemaining").innerText = guessesRemaining;
 // User wins total
-var userWins;
+var userWins = 0;
 // Store random word from randomChoice fxn
 var randomWord = randomChoice(wordArray);
 // Store already guessed letters in an array
-var alreadyChosenLetters = [];
+var alreadyGuessedLetters = [];
 // Variable to store correct letters to later check if user won or not
-var correctLetters;
-
+var correctLetters = "";
+// Hack!
+var randomChoiceUpper;
 
 // Function to return a random item in lower case from any passed array
 function randomChoice(myArray) {
-    var randomChoice = myArray[Math.floor(Math.random()*myArray.length)];
-    return randomChoice.toLowerCase();
+    randomChoiceUpper = myArray[Math.floor(Math.random()*myArray.length)];
+    return randomChoiceUpper.toLowerCase();
 }
 
 // Call to function to print underscores to page
 displayUnderscores(randomWord);
 
+// Function to create underscores on page for randomWord.length
+function displayUnderscores(randomWord_){
+    for (var i = 0; i < randomWord_.length; i++) {
+        // Creates spaces for space characters
+        if (randomWord_[i] == " ") {
+            var div = document.createElement("div");
+            div.className = "randomWordLetters";
+            div.id = "randomWordLetter" + i;
+            document.getElementById("currentWord").appendChild(div);
+            div.innerText = "\xa0\xa0\xa0\xa0";
+            // hack!
+            correctLetters += "a";
+            }
+        // Prints "___" for real letters.
+        else {
+            var div = document.createElement("div");
+            div.className = "randomWordLetters";
+            div.id = "randomWordLetter" + i;
+            document.getElementById("currentWord").appendChild(div);
+            div.innerText = "___ "
+        }
+
+    }
+}
+
 // Get user input and validate  / check for similarity between random word
 document.onkeyup = function(event){
-    console.log(event);
     if (validateInput(event)) {
         var userChoice = event.key.toLowerCase();
         if (checkRepeats(userChoice) == false) {
@@ -42,16 +69,7 @@ document.onkeyup = function(event){
 }
 
 
-// Function to create underscores on page for randomWord.length
-function displayUnderscores(randomWord_){
-    for (var i = 0; i < randomWord_.length; i++) {
-        var div = document.createElement("div");
-        div.className = "randomWordLetters";
-        div.id = "randomWordLetter" + i;
-        document.getElementById("currentWord").appendChild(div);
-        div.innerText = "___ "
-    }
-}
+
 
 // Function to validate user input is between A and Z
 function validateInput(event_){
@@ -64,12 +82,12 @@ function validateInput(event_){
 }
 // Function to check if user has already chosen the input
 function checkRepeats(userChoice_){
-    if (alreadyChosenLetters.includes(userChoice_)) {
+    if (alreadyGuessedLetters.includes(userChoice_)) {
         alert("You already chose the letter: " + userChoice_)
         return true;
     }
     else {
-        alreadyChosenLetters.push(userChoice_)
+        alreadyGuessedLetters.push(userChoice_)
         return false;
     }
 }
@@ -81,10 +99,49 @@ function checkContains(userChoice1_) {
         while (i != -1) {
             document.getElementById("randomWordLetter" + i).innerText = userChoice1_;
             i = randomWord.indexOf(userChoice1_, i + 1);
+            correctLetters += userChoice1_;
+            if (correctLetters.length == randomWord.length) {
+                youWin(randomChoiceUpper, randomWord);
+            }
         }
     }
     else {
         document.getElementById("incorrectLetters").innerHTML += "<h2>" + " " + userChoice1_ + "," + "</h2>";
         guessesRemaining--;
+        document.getElementById("guessesRemaining").innerText = guessesRemaining;
+        if (guessesRemaining == 0) {
+            youLose()
+        }
+        
     }
+}
+
+function youWin(randomWord1_, randomWord_) {
+    document.getElementById("musicName").innerHTML = "<h1 class = \"glyphicon glyphicon-music\">" + " " + randomWord1_ + "</h1>";
+    userWins++;
+    document.getElementById("wins").innerText = userWins;
+    document.getElementById("discImage").src = "assets/images/albumImages/" + randomWord_ + ".jpg"
+    resetGame(); 
+
+}
+
+function youLose(){
+    alert("Sorry! You ran out of chances!");
+    resetGame();
+}
+function resetGame() {
+// Maximum guesses available
+guessesRemaining = 12;
+document.getElementById("guessesRemaining").innerText = guessesRemaining;
+// Store random word from randomChoice fxn
+randomWord = randomChoice(wordArray);
+// Store already guessed letters in an array
+alreadyGuessedLetters = [];
+document.getElementById("incorrectLetters").innerText = "";
+// Variable to store correct letters to later check if user won or not
+correctLetters = "";
+// Hack!
+randomChoiceUpper;
+document.getElementById("currentWord").innerText = "";
+displayUnderscores(randomWord);
 }
